403033a4dc21eb6d71786437e7bc98430e7f5a6d
Malachi <malachi@mackie.co.nz>
2021-03-15 09:36:00 +1300

    Update Hover Tint

--- LogicGates2D/Assets/Scripts/GridTile.cs
+++ LogicGates2D/Assets/Scripts/GridTile.cs
@@ -5,21 +5,32 @@ using UnityEngine;
 
 public class GridTile : MonoBehaviour
 {
+    private bool _wired;
     private bool _hovered;
-    private bool _highlighted;
+    private Color _currentColor;
     private SpriteRenderer _spriteRenderer;
 
-    public Color baseColor = new Color(220, 210, 40, 0);
+    private Color _noHoverTint = new Color(1f, 1f, 1f, 0);
+    private Color _hoverTint;
+    private Color _noHoverWire = new Color(1f, 1f, 1f, 1);
+    private Color _hoverWire = new Color(220f / 255f, 210f / 255f, 40f / 255f, 1);
+
+    public Sprite wireSprite;
+    public Sprite baseSprite;
+
+    public Color hoverTint = new Color(220f / 255f, 210f / 255f, 40f / 255f);
 
     [Range(0, 1)] public float hoveredTransparency = 0.3f;
-
-    [Range(0, 1)] public float highlightedTransparency = 0.6f;
 
     // Start is called before the first frame update
     private void Start()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _spriteRenderer.color = baseColor;
+
+        Debug.Log(_hoverWire);
+
+        _hoverTint = new Color(hoverTint.r, hoverTint.g, hoverTint.b, hoveredTransparency);
+        _spriteRenderer.color = _noHoverTint;
     }
 
     // Update is called once per frame
@@ -30,24 +41,39 @@ public class GridTile : MonoBehaviour
 
     private void UpdateColor()
     {
-        var transparency = _highlighted
-            ? highlightedTransparency
-            : _hovered
-                ? hoveredTransparency
-                : 0;
-        _spriteRenderer.color = new Color(baseColor.r, baseColor.g, baseColor.b, transparency);
+        if (_hovered)
+        {
+            if (_wired)
+            {
+                _spriteRenderer.color = _hoverWire;
+            }
+            else
+            {
+                _spriteRenderer.color = _hoverTint;
+            }
+        }
+        else
+        {
+            if (_wired)
+            {
+                _spriteRenderer.color = _noHoverWire;
+            }
+            else
+            {
+                _spriteRenderer.color = _noHoverTint;
+            }
+        }
     }
 
     private void OnMouseEnter()
     {
         if (Input.GetMouseButton(0))
         {
-            _highlighted = true;
+            _wired = !_wired;
+            _spriteRenderer.sprite = wireSprite;
+            Debug.Log("Hello World");
         }
-        else
-        {
-            _hovered = true;
-        }
+        _hovered = true;
         UpdateColor();
     }
 
@@ -59,13 +85,17 @@ public class GridTile : MonoBehaviour
 
     private void OnMouseDown()
     {
-        _highlighted = true;
-        UpdateColor();
-    }
+        if (!_wired)
+        {
+            _wired = true;
+            _spriteRenderer.sprite = wireSprite;
+        }
+        else
+        {
+            _wired = false;
+            _spriteRenderer.sprite = baseSprite;
+        }
 
-    private void OnMouseUp()
-    {
-        _highlighted = false;
         UpdateColor();
     }
 }
